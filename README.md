145119fe65d049c6c8c1451a3b662eac80f91d4d
Kevin Tham <ktham@users.noreply.github.com>
2024-12-01 23:12:21 -0800

    Prepare repository for terraform-provider-postgresql development
    * Remove unnecessary files from project scaffolding
    * Update Github Actions to latest versions in `test.yml` workflow
      - Fix .golang.ci.yml outdated values
    * Update Go to 1.23.7
    * Update project dependencies:
      - github.com/hashicorp/terraform-plugin-docs v0.14.1 -> v0.21.0
      - github.com/hashicorp/terraform-plugin-framework v1.1.1 -> v.1.14.1
      - github.com/hashicorp/terraform-plugin-go v0.14.3 -> 0.26.0
      - github.com/hashicorp/terraform-plugin-log v0.8.0 -> v0.9.0
      - github.com/hashicorp/terraform-plugin-testing v1.1.0 -> v1.12.0
    * Rename files, variables and functions to Postgresql-specific names
    * Add initial placeholder `postgresql_role` resource and set up test harness

--- README.md
+++ README.md
@@ -1,35 +1,24 @@
-# Terraform Provider Scaffolding (Terraform Plugin Framework)
+# Terraform Provider for PostgreSQL
 
-_This template repository is built on the [Terraform Plugin Framework](https://github.com/hashicorp/terraform-plugin-framework). The template repository built on the [Terraform Plugin SDK](https://github.com/hashicorp/terraform-plugin-sdk) can be found at [terraform-provider-scaffolding](https://github.com/hashicorp/terraform-provider-scaffolding). See [Which SDK Should I Use?](https://www.terraform.io/docs/plugin/which-sdk.html) in the Terraform documentation for additional information._
-
-This repository is a *template* for a [Terraform](https://www.terraform.io) provider. It is intended as a starting point for creating Terraform providers, containing:
-
-- A resource and a data source (`internal/provider/`),
-- Examples (`examples/`) and generated documentation (`docs/`),
-- Miscellaneous meta files.
-
-These files contain boilerplate code that you will need to edit to create your own Terraform provider. Tutorials for creating Terraform providers can be found on the [HashiCorp Learn](https://learn.hashicorp.com/collections/terraform/providers-plugin-framework) platform. _Terraform Plugin Framework specific guides are titled accordingly._
-
-Please see the [GitHub template repository documentation](https://help.github.com/en/github/creating-cloning-and-archiving-repositories/creating-a-repository-from-a-template) for how to create a new repository from this template on GitHub.
-
-Once you've written your provider, you'll want to [publish it on the Terraform Registry](https://www.terraform.io/docs/registry/providers/publishing.html) so that others can use it.
+This Terraform provider is used to manage PostgreSQL objects.
 
 ## Requirements
 
-- [Terraform](https://www.terraform.io/downloads.html) >= 1.0
-- [Go](https://golang.org/doc/install) >= 1.19
+- [Terraform](https://www.terraform.io/downloads.html) >= 1.11
+- [Go](https://golang.org/doc/install) >= 1.23
+- [podman](https://golang.org/doc/install) >= 5.25
+    * On MacOS: Install from https://podman.io/
+    * On Ubuntu: `apt-get install -y podman`
+- [podman-compose](https://golang.org/doc/install) >= 1.2.0
+    * On MacOS: `brew install podman-compose`
+    * On Ubuntu: `apt-get install -y python3-pip` then `pip3 install podman-compose`
 
-## Building The Provider
+## Development
+### Building The Provider
 
-1. Clone the repository
-1. Enter the repository directory
-1. Build the provider using the Go `install` command:
+You can build the provider by running `make build`. It should build cleanly without errors.
 
-```shell
-go install
-```
-
-## Adding Dependencies
+### Adding Dependencies
 
 This provider uses [Go modules](https://github.com/golang/go/wiki/Modules).
 Please see the Go documentation for the most up to date information about using Go modules.
@@ -43,22 +32,37 @@ go mod tidy
 
 Then commit the changes to `go.mod` and `go.sum`.
 
-## Using the provider
-
-Fill this in for each provider
-
-## Developing the Provider
-
+### Building a local development version of the provider
 If you wish to work on the provider, you'll first need [Go](http://www.golang.org) installed on your machine (see [Requirements](#requirements) above).
 
-To compile the provider, run `go install`. This will build the provider and put the provider binary in the `$GOPATH/bin` directory.
+To compile the provider, run `make install`.
+This will build the provider and put the provider binary in the `$GOPATH/bin` directory.
 
-To generate or update documentation, run `go generate`.
+To use your locally built provider, you'll need to specify an override in your `~/.terraformrc` file:
 
-In order to run the full suite of Acceptance tests, run `make testacc`.
-
-*Note:* Acceptance tests create real resources, and often cost money to run.
-
-```shell
-make testacc
 ```
+provider_installation {
+  dev_overrides {
+     // Note: Replace `${GO_BIN_PATH}` with the fully qualified path on your system to your Go Bin directory
+     // For example: "
+    "ktham/postgresql" = "${GO_BIN_PATH}"
+  }
+
+  direct {}
+}
+```
+
+### Starting up a local Postgres database server
+You will also need to start a PostgreSQL container that can then be used for development and testing.
+
+```shell 
+podman compose up -d
+```
+
+### Testing
+To run the full suite of Acceptance tests, make sure you have a working Postgres server running, then run `make testacc`.
+
+## Generating documentation
+This provider uses terraform-plugin-docs to generate documentation and store it in the docs/ directory.
+
+Use `make generate` to ensure the documentation is regenerated with any changes.
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,80 +2,143 @@ package provider
 
 import (
 	"context"
-	"net/http"
-
+	"github.com/hashicorp/terraform-plugin-framework-validators/int32validator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// Ensure ScaffoldingProvider satisfies various provider interfaces.
-var _ provider.Provider = &ScaffoldingProvider{}
+var _ provider.Provider = &PostgresqlProvider{}
 
-// ScaffoldingProvider defines the provider implementation.
-type ScaffoldingProvider struct {
+type PostgresqlProvider struct {
 	// version is set to the provider version on release, "dev" when the
 	// provider is built and ran locally, and "test" when running acceptance
 	// testing.
 	version string
 }
 
-// ScaffoldingProviderModel describes the provider data model.
-type ScaffoldingProviderModel struct {
-	Endpoint types.String `tfsdk:"endpoint"`
+type PostgresqlProviderModel struct {
+	Hostname       types.String `tfsdk:"hostname"`
+	Port           types.Int32  `tfsdk:"port"`
+	DatabaseName   types.String `tfsdk:"database_name"`
+	Username       types.String `tfsdk:"username"`
+	Password       types.String `tfsdk:"password"`
+	MaxConnections types.Int32  `tfsdk:"max_connections"`
 }
 
-func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "scaffolding"
+func (p *PostgresqlProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "postgresql"
 	resp.Version = p.version
 }
 
-func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+func (p *PostgresqlProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"endpoint": schema.StringAttribute{
-				MarkdownDescription: "Example provider attribute",
-				Optional:            true,
+			"hostname": schema.StringAttribute{
+				Description: "The host name of the Postgres server.",
+				Required:    true,
+			},
+			"port": schema.Int32Attribute{
+				Description: "The TCP port on which Postgres is listening for connections.",
+				Required:    true,
+				Validators: []validator.Int32{
+					int32validator.Between(1, 65535),
+				},
+			},
+			"database_name": schema.StringAttribute{
+				Description: "The name of the database to connect to.",
+				Optional:    true,
+			},
+			"username": schema.StringAttribute{
+				Description: "The user used for connecting to Postgres.",
+				Required:    true,
+			},
+			"password": schema.StringAttribute{
+				Description: "The password to use for authentication.",
+				Optional:    true,
+				Sensitive:   true,
+			},
+			"max_connections": schema.Int32Attribute{
+				Description: "Maximum number of connections to establish to the database. Zero means unlimited.",
+				Optional:    true,
 			},
 		},
 	}
 }
 
-func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
+func (p *PostgresqlProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var config PostgresqlProviderModel
 
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
+	if config.Hostname.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("hostname"),
+			"Unknown Postgresql hostname value",
+			"The provider cannot create a connection to the Postgres server as `hostname` is an unknown configuration value",
+		)
+	}
+	if config.Port.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("port"),
+			"Unknown Postgresql port value",
+			"The provider cannot create a connection to the Postgres server as `port` is an unknown configuration value",
+		)
+	}
+	if config.DatabaseName.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("database_name"),
+			"Unknown Postgresql database_name value",
+			"The provider cannot create a connection to the Postgres server as `database_name` is an unknown configuration value",
+		)
+	}
+	if config.Username.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("username"),
+			"Unknown Postgresql username value",
+			"The provider cannot create a connection to the Postgres server as `username` is an unknown configuration value",
+		)
+	}
+	if config.Password.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("password"),
+			"Unknown Postgresql password value",
+			"The provider cannot create a connection to the Postgres server as `password` is an unknown configuration value",
+		)
+	}
+	if config.MaxConnections.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("max_connections"),
+			"Unknown Postgresql max_connections value",
+			"The provider cannot create a connection to the Postgres server as `max_connections` is an unknown configuration value",
+		)
+	}
 
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	resp.DataSourceData = nil // TODO
+	resp.ResourceData = nil   // TODO
 }
 
-func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
+func (p *PostgresqlProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		NewExampleResource,
+		NewRoleResource,
 	}
 }
 
-func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{
-		NewExampleDataSource,
-	}
+func (p *PostgresqlProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
 }
 
 func New(version string) func() provider.Provider {
 	return func() provider.Provider {
-		return &ScaffoldingProvider{
+		return &PostgresqlProvider{
 			version: version,
 		}
 	}
